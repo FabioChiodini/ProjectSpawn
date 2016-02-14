fdef4ca4a33753652c3d9639b74049bb62e24702
fabio.chiodini@gmail.com <ec2-user@ip-172-31-8-186.ec2.internal>
2016-02-14 16:37:17 +0000

    new line 6

--- SpawnK.sh
+++ SpawnK.sh
@@ -14,16 +14,26 @@ UUIDK=$(cat /proc/sys/kernel/random/uuid)
 docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION SPAWN-$UUIDK
 
 
-echo Container CREATED
+echo ---
+echo VM SPAWN-$UUIDK CREATED
+echo ---
 
 #Creates a docker container remotely
-docker-machine env SPAWN-$UUIDK
+#docker-machine env SPAWN-$UUIDK
+#eval $(docker-machine env SPAWN-$UUIDK)
+#eval $(docker-machine env)
+docker-machine env SPAWN-$UUIDK > /home/ec2-user/Docker1
+. /home/ec2-user/Docker1
 
 docker run --name docker-nginx -p 80:80 nginx
 
-echo connect to Public IP
 
 #Open port 80 on VM
+ws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 80 --cidr 0.0.0.0/0
+echo ----
+echo connect to Public IP
+echo ----
+
 
 
 #Gets rid of VM
