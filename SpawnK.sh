1c066cce949a38466cff051aa4de3aae2acb75ac
fabio.chiodini@gmail.com <ec2-user@ip-172-31-8-186.ec2.internal>
2016-02-14 15:36:23 +0000

    new line 4

--- SpawnK.sh
+++ SpawnK.sh
@@ -13,10 +13,21 @@ UUIDK=$(cat /proc/sys/kernel/random/uuid)
 #docker-machine create --driver amazonec2 
 docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION SPAWN-$UUIDK
 
+
+echo Container CREATED
+
+#Creates a docker container remotely
+docker-machine env SPAWN-$UUIDK
+
+docker run --name docker-nginx -p 80:80 nginx
+
+echo connect to Public IP
+
 #Open port 80 on VM
 
 
 #Gets rid of VM
+#Waits for a Y (to check if all is good)
 docker-machine rm SPAWN-$UUIDK
 
 
