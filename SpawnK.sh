f0f1f6be3c1436e5e773273f67f25c001eca14cc
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-02-21 16:42:47 +0100

    Update SpawnK.sh

--- SpawnK.sh
+++ SpawnK.sh
@@ -29,7 +29,7 @@ docker run --name docker-nginx -p 80:80 nginx
 
 
 #Open port 80 on VM
-ws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 80 --cidr 0.0.0.0/0
 echo ----
 echo connect to Public IP
 echo ----
